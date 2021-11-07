a65e8fac2742f33a17ff705ed372790c6040c9d2
Philippe Voinov <philippevoinov@gmail.com>
2021-11-07 13:07:16 +0100

    stuff

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,29 @@
 import * as React from "react";
+import { shuffle } from "lodash";
+import { zip } from "ramda";
 
-export const App = () => <div>walrus</div>;
+const enabledKeys = [
+  "KeyA",
+  "KeyS",
+  "KeyD",
+  "KeyF",
+  "KeyG",
+  "KeyH",
+  "KeyJ",
+  "KeyK",
+  "KeyL",
+  "Semicolon",
+];
+
+const alphabet = [];
+for (let i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++) {
+  alphabet.push(String.fromCharCode(i));
+}
+
+const randomLayout = new Map<string, string>(
+  zip(enabledKeys, shuffle(alphabet)),
+);
+
+export const App = () => (
+  <div>{JSON.stringify([...randomLayout.entries()])}</div>
+);
