cfacc52e47a06ee8ecc8a57555df8ca449629d51
Philippe Voinov <philippevoinov@gmail.com>
2021-11-07 13:13:50 +0100

    Correctly handle hot reload

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { shuffle } from "lodash";
+import { shuffle, sample } from "lodash";
 import { zip } from "ramda";
+import { useMemo, useState } from "react";
 
 const enabledKeys = [
   "KeyA",
@@ -15,15 +16,33 @@ const enabledKeys = [
   "Semicolon",
 ];
 
-const alphabet = [];
+const alphabet: string[] = [];
 for (let i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++) {
   alphabet.push(String.fromCharCode(i));
 }
 
-const randomLayout = new Map<string, string>(
-  zip(enabledKeys, shuffle(alphabet)),
-);
+function pickRandomMapEntry<K, V>(map: Map<K, V>): [K, V] {
+  if (!map.size) {
+    throw new Error("empty map");
+  }
+  const entries = [...map.entries()];
+  return sample(entries)!;
+}
 
-export const App = () => (
-  <div>{JSON.stringify([...randomLayout.entries()])}</div>
-);
+export const App = () => {
+  const randomLayout = useMemo(
+    () => new Map<string, string>(zip(enabledKeys, shuffle(alphabet))),
+    [],
+  );
+
+  const expectedLetter = useMemo(() => pickRandomMapEntry(randomLayout)[1], [
+    randomLayout,
+  ]);
+
+  return (
+    <div>
+      <div>{JSON.stringify([...randomLayout.entries()])}</div>
+      <div>Expected: {expectedLetter}</div>
+    </div>
+  );
+};
